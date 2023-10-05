33e05bfc427ca33476cfcfbb3f9ce66fdfec17d0
abdelrhmansalah44 <135262960+abdelrhmansalah44@users.noreply.github.com>
2023-10-06 00:15:53 +0300

    Delete unit8_mcu_interfacing/SPI/UART directory

--- unit8_mcu_interfacing/SPI/UART/lesson2 section/Debug/HAL/lcd.d
+++
@@ -1,4 +0,0 @@
-HAL/lcd.o HAL/lcd.o: ../HAL/lcd.c \
-  D:\ks\ embedded\ system\ ass\unit\ 8\ interfacing\lesson2\ section\HAL\inc/lcd.h
-
-D:\ks\ embedded\ system\ ass\unit\ 8\ interfacing\lesson2\ section\HAL\inc/lcd.h:
