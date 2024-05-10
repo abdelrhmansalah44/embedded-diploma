e51b5373ce4f8ef23e24412b1825c3e370a5b787
abdelrhmansalah44 <135262960+abdelrhmansalah44@users.noreply.github.com>
2024-05-10 23:48:50 +0300

    Add files via upload

---
+++ unit8_mcu_interfacing/i2c_stm32_driver/stm32_I2C_driver/Debug/HAL/I2C_EEPROM_slave.d
@@ -0,0 +1,11 @@
+HAL/I2C_EEPROM_slave.o: ../HAL/I2C_EEPROM_slave.c \
+ D:/ks\ embedded\ system\ ass/unit\ 8\ interfacing/I2C/i2c_lecture/stm32_I2C_driver/HAL/inc/I2C_EEPROM_slave.h \
+ D:/ks\ embedded\ system\ ass/unit\ 8\ interfacing/I2C/i2c_lecture/stm32_I2C_driver/STM32_drivers/inc/stm32_f103c6_I2C_driver.h \
+ D:/ks\ embedded\ system\ ass/unit\ 8\ interfacing/I2C/i2c_lecture/stm32_I2C_driver/STM32_drivers/inc/stm32f013x6.h \
+ D:/ks\ embedded\ system\ ass/unit\ 8\ interfacing/I2C/i2c_lecture/stm32_I2C_driver/STM32_drivers/inc/stm32_f103c6_gpio_driver.h \
+ D:/ks\ embedded\ system\ ass/unit\ 8\ interfacing/I2C/i2c_lecture/stm32_I2C_driver/STM32_drivers/inc/RCC_clock_configuration.h
+D:/ks\ embedded\ system\ ass/unit\ 8\ interfacing/I2C/i2c_lecture/stm32_I2C_driver/HAL/inc/I2C_EEPROM_slave.h:
+D:/ks\ embedded\ system\ ass/unit\ 8\ interfacing/I2C/i2c_lecture/stm32_I2C_driver/STM32_drivers/inc/stm32_f103c6_I2C_driver.h:
+D:/ks\ embedded\ system\ ass/unit\ 8\ interfacing/I2C/i2c_lecture/stm32_I2C_driver/STM32_drivers/inc/stm32f013x6.h:
+D:/ks\ embedded\ system\ ass/unit\ 8\ interfacing/I2C/i2c_lecture/stm32_I2C_driver/STM32_drivers/inc/stm32_f103c6_gpio_driver.h:
+D:/ks\ embedded\ system\ ass/unit\ 8\ interfacing/I2C/i2c_lecture/stm32_I2C_driver/STM32_drivers/inc/RCC_clock_configuration.h:
